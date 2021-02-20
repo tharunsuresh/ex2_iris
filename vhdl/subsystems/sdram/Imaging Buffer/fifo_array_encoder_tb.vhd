ec8c9ae16113ab5fe6b444e8dd1e35930fd0b409
Tharun Suresh <tharun.suresh@ualberta.ca>
2021-02-19 17:57:46 -0700

    working word 128 bit word encoder

--- vhdl/subsystems/sdram/Imaging Buffer/fifo_array_encoder_tb.vhd
+++ vhdl/subsystems/sdram/Imaging Buffer/fifo_array_encoder_tb.vhd
@@ -33,27 +33,35 @@ end entity fifo_array_encoder_tb;
 architecture rtl of fifo_array_encoder_tb is
 
     signal clock                : std_logic := '0'; 
-    --signal reset_n              : std_logic := '0'; 
+    signal reset_n              : std_logic := '0'; 
 
     signal vnir_row             : vnir.row_t := (others => "1111111111");
     signal vnir_row_fragment    : row_fragment_t;
-    signal num_bit              : natural;
-    signal num_pixel            : natural;
+    -- signal num_bit              : natural;
+    -- signal num_pixel            : natural;
  
 begin
 
     inst: entity work.fifo_array_encoder port map(
         clock           => clock,
-        --reset_n         => reset_n,
+        reset_n         => reset_n,
         vnir_row        => vnir_row,
-        num_bit         => num_bit,
-        num_pixel       => num_pixel,
+        -- num_bit         => num_bit,
+        -- num_pixel       => num_pixel,
         vnir_row_fragment => vnir_row_fragment
         );
 
+    reset_process: process
+    begin
+        reset_n <= '0';
+        wait for 50 ns; 
+        reset_n <= '1';
+        wait;
+    end process reset_process;
+
     clock <= NOT clock after 10 ns; 
-    num_bit     <= 0;
-    num_pixel   <= 0;
+    -- num_bit     <= 0;
+    -- num_pixel   <= 0;
 
     process is 
     begin
